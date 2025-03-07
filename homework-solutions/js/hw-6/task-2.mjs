e20c04d93bdc4cebabb1ab8ab5a5b28245fa0023
Marina-SA <sablinam89@gmail.com>
2025-03-07 22:33:34 +0100

    Добавлено решение hw-6 task-2

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,11 +13,55 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+let resultUnique = [];
+let resultNull = null; // присваеваем null что бы не было undefined
 
 const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
 const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
 const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+// приводим к нижнему регистру все пиццы
+let competitorPizzasLower = [];
+for (let i = 0; i < competitorPizzas.length; i++) {
+  competitorPizzasLower.push(competitorPizzas[i].toLowerCase());
+}
+let myPizzasT1Lower = [];
+for (let i = 0; i < myPizzasT1.length; i++) {
+  myPizzasT1Lower.push(myPizzasT1[i].toLowerCase());
+}
+let myPizzasT2Lower = []; 
+for (let i = 0; i < myPizzasT2.length; i++) {
+  myPizzasT2Lower.push(myPizzasT2[i].toLowerCase());
+}
+
+// проверяем, какие пиццы есть только у нас 
+for (let i = 0; i < myPizzasT1Lower.length; i++) {
+  let pizzaName = myPizzasT1Lower[i];
+
+  if (!competitorPizzasLower.includes(pizzaName)) { 
+    resultUnique.push(myPizzasT1[i]); 
+  }
+}
+
+// Если у нас нет уникальных пицц, устанавливаем `resultNull`
+if (resultUnique.length === 0) {
+  resultNull = "null";
+}
+
+// myPizzasT2 - проверяем, какие пиццы есть только у нас 
+let resultUniqueT2 = [];
+for (let i = 0; i < myPizzasT2Lower.length; i++) {
+  let pizzaName = myPizzasT2Lower[i]; 
+
+  if (!competitorPizzasLower.includes(pizzaName)) { 
+    resultUniqueT2.push(myPizzasT2[i]); 
+  }
+}
+
+
+// Выводим результат
+console.log("Уникальные пиццы (T1):", resultUnique);
+console.log("Результат (T1):", resultNull);
+console.log("Уникальные пиццы (T2):", resultUniqueT2);
 
 export { resultNull, resultUnique };
